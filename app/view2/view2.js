d3de6ae2d5a91ea03d1052ab46570d20d8e50a06
Luciano Duarte <luciano0509@gmail.com>
2015-10-25 23:04:30 -0300

    ngInfininityScroll

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view2', ['ui.router'])
+angular.module('myApp.view2', ['ui.router', 'infinite-scroll'])
 
 .config(function($stateProvider) {
   $stateProvider.state('view2', {
@@ -10,7 +10,37 @@ angular.module('myApp.view2', ['ui.router'])
   });
 })
 
-.controller('View2Ctrl', function($scope) {
-    $scope.variableCtrl2 = 'contenido del la variable 2';
-        console.log("controlador 2")
+.controller('View2Ctrl', function($scope, $http) {
+        $scope.variableCtrl2 = 'contenido del la variable 2';
+        console.log("controlador 2");
+
+        $scope.list = [1,2,3,4,5,6];
+
+        $scope.addTweets = function() {
+            var last = $scope.images[$scope.images.length - 1];
+            for(var i = 1; i <= 8; i++) {
+                $scope.images.push(last + i);
+            }
+        };
+
+        //if ($scope.list.length === 0) {
+        //    $http({
+        //        method: "GET",
+        //        url: "http://127.0.0.1:3000/tweets/+"+5
+        //    }).then(function (result) {
+        //        $scope.list = result.data
+        //    })
+        //}
+        //
+        //$scope.addTweets = function () {
+        //    $http({
+        //        method: "GET",
+        //        url: "http://127.0.0.1:3000/tweets/+"+5
+        //    }).then(function (result) {
+        //        for (var tweet of result) {
+        //            $scope.list.push(tweet);
+        //        }
+        //    })
+        //};
+
 });
